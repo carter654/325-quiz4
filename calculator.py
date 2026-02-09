14df6d417e0344ce3e58e70cc14f73cdf4e3584c
tyhamma <tyhamma@siue.edu>
2026-02-08 22:56:52 -0600

    base calculator file

---
+++ calculator.py
@@ -0,0 +1,14 @@
+while True:
+	print("\n" * 500)
+	print("=========SIMPLE CALCULATOR PROGRAM=========\n")
+
+	print("\nEnter:\n    1 for addition\n    2 for subtraction\n    3 for multiplication\n    4 for division\n    0 to exit\n\n")
+
+	option = input("===========================================\nintput: ")
+
+
+
+	if option == "0":
+		print("\n" * 500)
+		exit(0)
+		
